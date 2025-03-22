10cdc3c9c01702c7f27e06e08df5c136e6caf904
loading-hh <loading-hh@qq.com>
2025-03-22 18:49:08 +0800

    yolo模型整体写完。

---
+++ my_yolo.py
@@ -0,0 +1,90 @@
+import tqdm
+import torch
+import torchvision
+from PIL.ImageFont import load_path
+from torch import nn
+from torch.utils import data
+from torchvision import transforms
+import matplotlib.pyplot as plt
+from IPython import display
+
+
+class Convolutional(nn.Module):
+    '''
+    description: 搭建Convolutional模块
+    param {*} self
+    param {*} in_channels是输入特征映射图的通道数
+    param {*} out_channels是输出特征映射图的通道数
+    param {*} kernel_size是卷积核的大小
+    param {*} padding是否有填充,有的话为多少
+    return {*}
+    '''
+    def __init__(self, in_channels, out_channels, kernel_size, padding):
+        super().__init__()
+        self.conv2d = nn.Conv2d(in_channels, out_channels, kernel_size, stride = 1, padding = padding)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.act = nn.ReLU()
+
+    def forward(self, x):
+        x = self.conv2d(x)
+        x = self.bn(x)
+        x = self.act(x)
+
+        return x
+
+
+class ConvolutionalBlock(nn.Module):
+    '''
+    description: 搭建Convolutional块,因为是多个Convolutional块在一块的.
+    param {*} self
+    param {*} block_num有几个Convolutional块
+    param {*} in_channels是每个Convolutional的输入通道数
+    param {*} out_channels是每个Convolutional的输出通道数
+    param {*} kernel_sizes是每个Convolutional的卷积核大小
+    param {*} paddings是每个Convolutional的填充大小
+    return {*}
+    '''
+    def __init__(self, block_num, in_channels, out_channels, kernel_sizes, paddings):
+        super().__init__()
+        self.block_num = block_num
+        for i in range(self.block_num):
+            setattr(self, f"blk_{i}", Convolutional(in_channels[i], out_channels[i], kernel_sizes[i], paddings[i]))
+
+    def forward(self, x):
+        for i in range(self.block_num):
+            x = getattr(self, f"blk_{i}")(x)
+
+        return x
+
+
+class Yolo(nn.Module):
+    def __init__(self, block_list, in_channels, out_channels, kernel_sizes, paddings, N):
+        super().__init__()
+        self.block_num = len(block_list)
+        self.pool = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        self.conv = nn.Conv2d(out_channels[-1][-1], N, 1)
+
+        for i in range(self.block_num):
+            if i < (self.block_num - 1):
+                setattr(self, f"blk_{i}", nn.Sequential(ConvolutionalBlock(block_list[i], in_channels[i], out_channels[i], kernel_sizes[i], paddings[i]), self.pool))
+            else:
+                setattr(self, f"blk_{i}", ConvolutionalBlock(block_list[i], in_channels[i], out_channels[i], kernel_sizes[i], paddings[i]))
+
+
+    def forward(self, x):
+        for i in range(self.block_num):
+            x = getattr(self, f"blk_{i}")(x)
+        x = self.conv(x)
+
+        return x
+
+
+if __name__ == "__main__":
+    block_list = [1, 1, 3, 3, 5, 5]
+    in_channels = [[3], [32], [64, 128, 64], [128, 256, 128], [256, 512, 256, 512, 256], [512, 1024, 512, 1024, 512]]
+    out_channels = [[32], [64], [128, 64, 128], [256, 128, 256], [512, 256, 512, 256, 512], [1024, 512, 1024, 512, 1024]]
+    kernel_sizes = [[3], [3], [3, 1, 3], [3, 1, 3], [3, 1, 3, 1, 3], [3, 1, 3, 1, 3]]
+    paddings = [[1], [1], [1, 0, 1], [1, 0, 1], [1, 0, 1, 0, 1], [1, 0, 1, 0, 1]]
+    net = Yolo(block_list, in_channels, out_channels, kernel_sizes, paddings, 12)
+    x = torch.rand((1, 3, 416, 416))
+    print(net(x).shape)
